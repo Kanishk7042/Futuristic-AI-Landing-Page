85eec6c7e046cb71832d0b6882a8500fd1d0be71
Kanishk7042 <kanishkaswal14@gmail.com>
2024-11-21 13:20:20 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import Companies from "@/sections/Companies";
+import { Header } from "@/sections/Header";
+import Hero from "@/sections/Hero";
+import Features from "../sections/Features";
+import Pricing from "@/sections/Pricing";
+import Testimonials from "@/sections/Testimonials";
+import CallToAction from "@/sections/CallToAction";
+import Footer from "@/sections/Footer";
+
+export default function Home() {
+  return(  <>
+  <Header/>
+  <Hero/>
+  <Companies/>
+  <Features/>
+  <Pricing/>
+  <Testimonials/>
+  <CallToAction/>
+  <Footer/>
+    </>
+  );
+}
